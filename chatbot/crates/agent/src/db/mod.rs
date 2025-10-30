781481372b06ac7726fedf4b1345f6afd3de878a
kevinssgh <kevinssgh.github@gmail.com>
2025-10-30 16:09:44 -0400

    added unit and integration test for contract store

--- chatbot/crates/agent/src/db/mod.rs
+++ chatbot/crates/agent/src/db/mod.rs
@@ -1,9 +1,6 @@
 mod contract_store;
 mod traits;
 
-pub use contract_store::ContractStore;
-pub use traits::ContractStoreApi;
-
 // Domain model
 #[derive(Debug, Clone)]
 pub struct Contract {
@@ -15,6 +12,24 @@ pub struct Contract {
 
 impl<'r> sqlx::FromRow<'r, sqlx::any::AnyRow> for Contract {
     fn from_row(row: &'r sqlx::any::AnyRow) -> Result<Self, sqlx::Error> {
+        use sqlx::Row;
+        Ok(Contract {
+            address: row.try_get("address")?,
+            chain: row.try_get("chain")?,
+            source_code: row.try_get("source_code")?,
+            abi: serde_json::from_str(row.try_get("abi")?).map_err(|e| {
+                sqlx::Error::ColumnDecode {
+                    index: "abi".to_string(),
+                    source: Box::new(e),
+                }
+            })?,
+        })
+    }
+}
+
+#[cfg(test)]
+impl<'r> sqlx::FromRow<'r, sqlx::sqlite::SqliteRow> for Contract {
+    fn from_row(row: &'r sqlx::sqlite::SqliteRow) -> Result<Self, sqlx::Error> {
         use sqlx::Row;
         Ok(Contract {
             address: row.try_get("address")?,
